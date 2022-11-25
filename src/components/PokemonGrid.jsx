ae430cd8943cfe71c79adca2121430944799321f
luqmanhxc <luqmanhxc@gmail.com>
2022-11-26 00:58:04 +0800

    fix: rename hook to useAxios

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -1,17 +1,20 @@
 import PokemonPreview from "./PokemonPreview";
-import useFetch from "../hooks/useAxios";
+import useAxios from "../hooks/useAxios";
+import { Link } from "react-router-dom";
 
 const PokemonGrid = () => {
-  const { pokeData } = useFetch();
+  const { pokeData } = useAxios();
+  // console.log(pokeData);
   return (
     <div className="grid grid-cols-4  gap-4 p-10">
       {pokeData.map((p) => (
-        <PokemonPreview
-          key={p.id}
-          id={p.id}
-          name={p.name}
-          sprite={p.sprites.front_default}
-        />
+        <Link to={`/${p.id}`} state={{ data: p }} key={p.id}>
+          <PokemonPreview
+            id={p.id}
+            name={p.name}
+            sprite={p.sprites.front_default}
+          />
+        </Link>
       ))}
     </div>
   );
