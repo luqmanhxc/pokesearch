6ecd8810c7b27ca578551b8639b876ba40431740
luqmanhxc <luqmanhxc@gmail.com>
2022-11-28 01:38:27 +0800

    fix: change dynamic routing

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -1,14 +1,11 @@
 import PokemonPreview from "./PokemonPreview";
-import useAxios from "../hooks/useAxios";
 import { Link } from "react-router-dom";
 
-const PokemonGrid = () => {
-  const { pokeData } = useAxios();
-  // console.log(pokeData);
+const PokemonGrid = ({ pokeData }) => {
   return (
     <div className="grid grid-cols-4  gap-4 p-10">
       {pokeData.map((p) => (
-        <Link to={`/${p.id}`} state={{ data: p }} key={p.id}>
+        <Link to={`/pokemon/${p.id}`} state={{ data: p }} key={p.id}>
           <PokemonPreview
             id={p.id}
             name={p.name}
