d2c27aef480442a092af0c615aab290b21ce4a4f
luqmanhxc <luqmanhxc@gmail.com>
2022-11-28 01:38:45 +0800

    fix: update stats component styling

--- src/components/pokemonPage/Stats.jsx
+++ src/components/pokemonPage/Stats.jsx
@@ -7,13 +7,25 @@ const Stats = ({
   speed,
 }) => {
   return (
-    <div className="mt-5 grid grid-cols-3 gap-10 p-5 text-center text-2xl">
-      <p className="">Health: {hp}</p>
-      <p>Attack: {attack}</p>
-      <p>Defense: {defense}</p>
-      <p>Special Attack: {specialAttack}</p>
-      <p>Special Defense: {specialDefense}</p>
-      <p>Speed: {speed}</p>
+    <div className="mt-5 grid grid-cols-3 gap-10 rounded-lg border-2 p-5 text-center text-xl tracking-wider">
+      <p>
+        HEALTH <span className="block text-4xl ">{hp}</span>
+      </p>
+      <p>
+        ATTACK <span className="block text-4xl ">{attack}</span>
+      </p>
+      <p>
+        DEFENSE <span className="block text-4xl ">{defense}</span>
+      </p>
+      <p>
+        SP. ATTACK <span className="block text-4xl ">{specialAttack}</span>
+      </p>
+      <p>
+        SP. DEFENSE <span className="block text-4xl ">{specialDefense}</span>
+      </p>
+      <p>
+        SPEED <span className="block text-4xl ">{speed}</span>
+      </p>
     </div>
   );
 };
