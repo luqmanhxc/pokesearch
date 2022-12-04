63359509eb0665375ac8ee0bc19b6ee97ae703a8
luqmanhxc <luqmanhxc@gmail.com>
2022-12-05 00:34:44 +0800

    fix: change .env import method for vite

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
