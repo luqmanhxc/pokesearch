8ff67ee8addccb5a8c93db5140027c6674ee3bb7
luqmanhxc <luqmanhxc@gmail.com>
2022-12-07 23:07:25 +0800

    feat: create Layout component

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/layout/Navbar";
+import Footer from "../components/layout/Footer";
+
+const Layout = (props) => {
+  return (
+    <>
+      <NavBar />
+      <main className="bg-slate-600">{props.children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
