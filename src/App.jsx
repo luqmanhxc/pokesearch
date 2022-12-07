a3d81b102c24c8610237e119d166b38c8057dd94
luqmanhxc <luqmanhxc@gmail.com>
2022-12-07 23:16:41 +0800

    fix: refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import PokemonPage from "./pages/PokemonPage";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import Layout from "./layout/Layout";
 import PokemonGrid from "./components/PokemonGrid";
 import Pagination from "./components/Pagination";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import LoginButton from "./components/buttons/login";
-import LogoutButton from "./components/buttons/logout";
 import Profile from "./pages/Profile";
 import { BrowserRouter } from "react-router-dom";
 
@@ -50,39 +48,35 @@ const App = () => {
   };
 
   return (
-    <div className="h-full bg-slate-900">
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div>
-                <NavBar>
-                  <LoginButton />
-                  <LogoutButton />
-                  <PokemonGrid pokeData={pokeData} />
-                  <Pagination
-                    prevUrl={prevUrl}
-                    nextUrl={nextUrl}
-                    goToPrevPage={goToPrevPage}
-                    goToNextPage={goToNextPage}
-                  />
-                </NavBar>
-              </div>
-            }
-          />
-          <Route
-            path="/pokemon/:id"
-            element={
-              <NavBar>
-                <PokemonPage />
-              </NavBar>
-            }
-          />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Layout>
+                <PokemonGrid pokeData={pokeData} />
+                <Pagination
+                  prevUrl={prevUrl}
+                  nextUrl={nextUrl}
+                  goToPrevPage={goToPrevPage}
+                  goToNextPage={goToNextPage}
+                />
+              </Layout>
+            </div>
+          }
+        />
+        <Route
+          path="/pokemon/:id"
+          element={
+            <Layout>
+              <PokemonPage />
+            </Layout>
+          }
+        />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
