b80b83b2e927560a7f81af3eb81038fbb7f2309d
luqmanhxc <luqmanhxc@gmail.com>
2022-12-08 04:38:39 +0800

    fix: moved api state and function to its own hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import PokemonGrid from "./components/PokemonGrid";
 import Pagination from "./components/Pagination";
-import axios from "axios";
-import { useState, useEffect } from "react";
 import Profile from "./pages/Profile";
 import { BrowserRouter } from "react-router-dom";
+import usePokemonData from "./hooks/usePokemonData";
 
 const App = () => {
-  const [pokeData, setPokeData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
-  const [nextUrl, setNextUrl] = useState();
-  const [prevUrl, setPrevUrl] = useState();
-
-  const getPokemonData = async () => {
-    setPokeData([]);
-    setLoading(true);
-    const res = await axios.get(url);
-    setNextUrl(res.data.next);
-    setPrevUrl(res.data.previous);
-    getPokemonDetails(res.data.results);
-    setLoading(false);
-  };
-
-  const getPokemonDetails = async (res) => {
-    res.map(async (item) => {
-      const result = await axios.get(item.url);
-      setPokeData((state) => {
-        state = [...state, result.data];
-        return state;
-      });
-    });
-  };
-
-  useEffect(() => {
-    getPokemonData();
-  }, [url]);
-
-  const goToPrevPage = () => {
-    setUrl(prevUrl);
-  };
-
-  const goToNextPage = () => {
-    setUrl(nextUrl);
-  };
+  const { pokeData, loading, prevUrl, nextUrl, goToPrevPage, goToNextPage } =
+    usePokemonData();
 
   return (
     <BrowserRouter>
@@ -57,10 +21,10 @@ const App = () => {
               <Layout>
                 <PokemonGrid pokeData={pokeData} />
                 <Pagination
-                  prevUrl={prevUrl}
-                  nextUrl={nextUrl}
                   goToPrevPage={goToPrevPage}
                   goToNextPage={goToNextPage}
+                  prevUrl={prevUrl}
+                  nextUrl={nextUrl}
                 />
               </Layout>
             </div>
