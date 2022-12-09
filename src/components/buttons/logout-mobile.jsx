16f3938bd0687a532eeda889f3780586ab3f06e3
luqmanhxc <luqmanhxc@gmail.com>
2022-12-09 18:30:44 +0800

    feat: added component for mobile logout button

---
+++ src/components/buttons/logout-mobile.jsx
@@ -0,0 +1,17 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+
+const LogoutButtonMobile = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button
+      onClick={() => logout({ returnTo: window.location.origin })}
+      className="rounded bg-white py-2 px-4 text-center text-black hover:bg-gray-200"
+    >
+      Log Out
+    </button>
+  );
+};
+
+export default LogoutButtonMobile;
