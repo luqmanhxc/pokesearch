893f6063185fbd8e814d97ead6837cd38f9c985a
luqmanhxc <luqmanhxc@gmail.com>
2022-12-09 18:30:14 +0800

    feat: added component for mobile login button

---
+++ src/components/buttons/login-mobile.jsx
@@ -0,0 +1,17 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+
+const LoginButtonMobile = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <button
+      onClick={() => loginWithRedirect()}
+      className="mb-4 rounded bg-white py-2 px-4 text-center text-black hover:bg-gray-200"
+    >
+      Log In
+    </button>
+  );
+};
+
+export default LoginButtonMobile;
