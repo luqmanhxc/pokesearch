f860cd552aac313804328087ff329fd5c86f2a74
luqmanhxc <luqmanhxc@gmail.com>
2022-12-10 01:14:06 +0800

    fix: sort pokemon by ID

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 const PokemonGrid = ({ pokeData }) => {
   return (
     <div className="grid  grid-cols-2 gap-4 p-10 md:grid-cols-4">
-      {pokeData?.map((p) => (
-        <Link to={`/pokemon/${p.id}`} state={{ data: p }} key={p.id}>
-          <PokemonPreview
-            id={p.id}
-            name={p.name}
-            sprite={p.sprites.front_default}
-            type={p.types[0].type.name}
-          />
-        </Link>
-      ))}
+      {pokeData
+        ?.sort((a, b) => (a.id > b.id ? 1 : -1))
+        .map((p) => (
+          <Link to={`/pokemon/${p.id}`} state={{ data: p }} key={p.id}>
+            <PokemonPreview
+              id={p.id}
+              name={p.name}
+              sprite={p.sprites.front_default}
+              type={p.types[0].type.name}
+            />
+          </Link>
+        ))}
     </div>
   );
 };
