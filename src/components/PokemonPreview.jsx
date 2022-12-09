c4d25e38c68f881cf77cfe1e5119740a72c02f54
luqmanhxc <luqmanhxc@gmail.com>
2022-12-10 06:11:20 +0800

    Update PokemonPreview.jsx

--- src/components/PokemonPreview.jsx
+++ src/components/PokemonPreview.jsx
@@ -21,15 +21,3 @@ const PokemonPreview = ({ id, name, sprite, type }) => {
 };
 
 export default PokemonPreview;
-
-// {
-//   /*<div
-//     key={id}
-//     className="mx-auto flex w-full cursor-pointer items-center justify-evenly justify-self-center rounded-lg bg-slate-800 px-2 py-2 shadow-lg transition duration-200 hover:scale-105"
-//   >
-//     <img src={sprite} className="w-30 h-30 rounded-full bg-indigo-500" />
-//     <h1 className="font-inter text-lg text-white">
-//       {name.charAt(0).toUpperCase() + name.slice(1)}
-//     </h1>
-//   </div>*/
-// };
