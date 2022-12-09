7887e6982814b1c090ee392a1a71538a2f4b38f5
luqmanhxc <luqmanhxc@gmail.com>
2022-12-10 06:31:17 +0800

    feat: add loading spinner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,26 @@ import Layout from "../layout/Layout";
 import PokemonGrid from "../components/PokemonGrid";
 import Pagination from "../components/Pagination";
 import usePokemonData from "../hooks/usePokemonData";
+import Spinner from "../components/Spinner";
 
 const Home = () => {
   const { pokeData, loading, prevUrl, nextUrl, goToPrevPage, goToNextPage } =
     usePokemonData();
   return (
     <Layout>
-      <PokemonGrid pokeData={pokeData} />
-      <Pagination
-        goToPrevPage={goToPrevPage}
-        goToNextPage={goToNextPage}
-        prevUrl={prevUrl}
-        nextUrl={nextUrl}
-      />
+      {pokeData.length < 20 ? (
+        <Spinner />
+      ) : (
+        <>
+          <PokemonGrid pokeData={pokeData} />
+          <Pagination
+            goToPrevPage={goToPrevPage}
+            goToNextPage={goToNextPage}
+            prevUrl={prevUrl}
+            nextUrl={nextUrl}
+          />
+        </>
+      )}
     </Layout>
   );
 };
