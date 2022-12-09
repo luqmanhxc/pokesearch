311accfb6d34b2cf4db98d706f3407c6f0dcb803
luqmanhxc <luqmanhxc@gmail.com>
2022-12-09 18:31:47 +0800

    feat: passed type as props

--- src/components/PokemonGrid.jsx
+++ src/components/PokemonGrid.jsx
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 
 const PokemonGrid = ({ pokeData }) => {
   return (
-    <div className="grid grid-cols-2  gap-4 p-10 md:grid-cols-4">
+    <div className="grid  grid-cols-2 gap-4 p-10 md:grid-cols-4">
       {pokeData.map((p) => (
         <Link to={`/pokemon/${p.id}`} state={{ data: p }} key={p.id}>
           <PokemonPreview
             id={p.id}
             name={p.name}
             sprite={p.sprites.front_default}
+            type={p.types[0].type.name}
           />
         </Link>
       ))}
