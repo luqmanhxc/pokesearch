9d2f0d57460b81e85ee3ea35acee9b13b670b215
luqmanhxc <luqmanhxc@gmail.com>
2022-12-09 18:29:49 +0800

    feat: added protectedroute to profile

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Layout from "./layout/Layout";
 import PokemonGrid from "./components/PokemonGrid";
 import Pagination from "./components/Pagination";
 import Profile from "./pages/Profile";
+import About from "./pages/About";
 import { BrowserRouter } from "react-router-dom";
 import usePokemonData from "./hooks/usePokemonData";
 import Spinner from "./components/spinner";
+import ProtectedComponent from "./components/ProtectedRoute";
 
 const App = () => {
   const { pokeData, loading, prevUrl, nextUrl, goToPrevPage, goToNextPage } =
@@ -45,7 +47,11 @@ const App = () => {
             </Layout>
           }
         />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/profile"
+          element={<ProtectedComponent component={Profile} />}
+        />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
