03459213b7c9c588d28887c4a3d53be06e9c13e1
luqmanhxc <luqmanhxc@gmail.com>
2022-12-11 01:45:08 +0800

    feat: added errorpage when fetch fails

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <Link to="/">
+      <main className="flex h-screen w-full flex-col items-center justify-center bg-[#1A2238]">
+        <h1 className="text-9xl font-extrabold tracking-widest text-white">
+          404
+        </h1>
+        <div className="absolute rotate-12 rounded bg-[#FF6A3D] px-2 text-sm">
+          Pokemon Not Found
+        </div>
+        <button className="mt-5">
+          <div className="group relative inline-block text-sm font-medium text-[#FF6A3D] focus:outline-none focus:ring active:text-orange-500">
+            <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] transition-transform group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+            <span className="relative block border border-current bg-[#1A2238] px-8 py-3">
+              <p>Go Home</p>
+            </span>
+          </div>
+        </button>
+      </main>
+    </Link>
+  );
+};
+
+export default ErrorPage;
