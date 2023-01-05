89bd3579e8d50af8124a166aac14f6f465c77950
luqmanhxc <luqmanhxc@gmail.com>
2023-01-06 01:32:28 +0800

    fix: remove search case sensitive

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,7 +8,10 @@ const SearchSection = () => {
   const handleSearch = (event) => {
     event.preventDefault();
     if (value) {
-      navigate(`/search/${value}`, { state: value, replace: true });
+      navigate(`/search/${value.toLowerCase()}`, {
+        state: value.toLowerCase(),
+        replace: true,
+      });
     }
   };
 
@@ -20,7 +23,7 @@ const SearchSection = () => {
             className="my-3 block w-[350px] rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
             id="input-search-header"
             value={value}
-            onChange={(event) => setValue(event.target.value)}
+            onChange={(event) => setValue(event.target.value.toLowerCase())}
             placeholder="Search Pokemon or ID"
           />
           <button
